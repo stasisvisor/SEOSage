049fa76b6c5b2d3c22d9c21c3cdb1fdd39e87136
stasisvisor <stasis_visor.02@icloud.com>
2024-08-26 19:30:00 +0700

    Improved accessibility features

---
+++ pan.js
@@ -0,0 +1,1 @@
+const throttle = (func, limit) => { let inThrottle; return function(...args) { if (!inThrottle) { func.apply(this, args); inThrottle = true; setTimeout(() => inThrottle = false, limit); } }; };
